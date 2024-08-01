e5637ea31bc670f0e5d2cbd1da0f9cdd8a13ffc2
João Victor <joaovictor2010lima@gmail.com>
2024-07-31 23:54:16 -0300

    Create aula08.py

---
+++ Aulas/aula08.py
@@ -0,0 +1,12 @@
+nome = 'João'
+sobrenome = 'lima'
+idade = 21
+nascimento = 2003
+maior_de_idade = idade >=18
+altura = 1.84
+print('Nome:', nome)
+print('Sobrenome: ' , sobrenome)
+print('Idade: ',idade)
+print('Ano de nascimento: ',nascimento)
+print('É maior de idade? ',maior_de_idade)
+print('Altura em metros: ',altura)

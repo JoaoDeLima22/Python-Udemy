c2aab3ba3bf57879e2e6533f36da2170e9ca8588
João Victor <joaovictor2010lima@gmail.com>
2024-08-21 23:47:19 -0300

    Update aula13.py

--- Aulas/aula13.py
+++ Aulas/aula13.py
@@ -1,10 +1,17 @@
-a = 'AAA'
-b = 'BB'
-c = 1.1
-formato = 'a={} b={} c={:.2f}'.format(a,b,c)
+nome = 'João Victor'
+altura = 1.84
+peso = 92.0
+imc = peso / altura ** 2
 
-print(formato)
+"f-strings"
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
 
-string = 'a={} b={} c={:.2f}   {}'
-formato2 = string.format(a,b,c, 123)
-print(formato2)
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987

8725e7be739396c1019ad52b1e7975bf2fa31559
João Victor <joaovictor2010lima@gmail.com>
2024-09-18 12:19:32 -0300

    Update automacao.py

--- Aulas/automacao.py
+++ Aulas/automacao.py
@@ -1,12 +1,16 @@
 import os
 
-# Definir o número inicial e o número final desejado
-inicio = 22
-fim = 31 # Substitua pelo número final desejado
+# Defina o número inicial e o número final desejado
+inicio = 32
+fim = 50  # Substitua pelo número final desejado
 
-# Loop para criar os arquivos
-for i in range(inicio, fim +1):
-    nome_arquivo = f'aula{i:03}.py' # O {03} mantém os números com 3 dígitos (ex: aula021.py)
-    with open(nome_arquivo, 'w') as arquivo:
-        arquivo.write(f'# Arquivo {nome_arquivo}\n')
-    print(f'Arquivo {nome_arquivo} criado.')
+# Obtém o diretório atual (onde o script está sendo executado)
+diretorio_atual = os.getcwd()
+
+# Loop para criar os arquivos no diretório atual
+for i in range(inicio, fim + 1):
+    nome_arquivo = f'aula{i}.py'
+    caminho_completo = os.path.join(diretorio_atual, nome_arquivo)  # Cria o caminho completo no diretório atual
+    with open(caminho_completo, 'w') as arquivo:
+        arquivo.write(f'# Arquivo {nome_arquivo}\n')  # Escreve um comentário no início do arquivo
+    print(f'Arquivo {nome_arquivo} criado em {diretorio_atual}.')

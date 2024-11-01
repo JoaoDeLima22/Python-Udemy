007f0ecacafe2654ccd1f0f3e2e4d94eebdbeb06
João Victor <joaovictor2010lima@gmail.com>
2024-10-31 23:42:42 -0300

    Update aula54.py

--- Aulas/aula54.py
+++ Aulas/aula54.py
@@ -6,13 +6,15 @@ Não permita que o programa quebre com
 erros de índices inexistentes na lista.
 """
 # Inicializando a lista de compras
+import os
 compras = []
 while True:
     print("Selecione uma opção")
     opcao= input("[i]nserir   [a]pagar   [l]istar:")
-    
+     
     if opcao in ["i","a","l"]:
         if opcao =="l":
+            print("Lista de Produtos: ")
             if compras !=[]:
                 for item in enumerate(compras):
                     indice, produto = item
@@ -20,9 +22,11 @@ while True:
             else:
                 print("Lista de compras vazia")
         elif opcao == "i":
+            os.system('cls')
             produto = input("Digite o produto que deseja adicionar:")
             compras.append(produto)
         elif opcao == "a":
+            os.system('cls')
             if compras !=[]:             
                 try:
                     indice = int(input("Digite o índice do produto que deseja apagar:"))
@@ -31,4 +35,6 @@ while True:
                     print("Índice inexistente")
             else:
                 print("Lista de compras vazia")
+        else:
+            print("Opção inválida! Por Favor escola i, a ou l")
     

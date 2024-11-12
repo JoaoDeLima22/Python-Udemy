1f30173ef2085e23fa0bfb9a1c822be42ea5d857
João Victor <joaovictor2010lima@gmail.com>
2024-11-12 10:22:40 -0300

    Create aula56.py

---
+++ Aulas/aula56.py
@@ -0,0 +1,19 @@
+""" 
+split e join com list e str
+split - divide uma string
+join - une uma string
+"""
+frase = 'Olha só que, coisa interessante'
+lista_palavras = frase.split()
+print(lista_palavras)
+lista_frases = frase.split(',')
+print(lista_frases)
+
+for i,frase in enumerate(lista_frases):
+    print(lista_frases[i].strip(  ))
+    
+frases_unidas = '-'.join('abc')
+print(frases_unidas)
+
+frases_unidas = ', '.join(lista_frases)
+print(frases_unidas)
